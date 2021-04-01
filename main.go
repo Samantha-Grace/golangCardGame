e4ae307469fd2dc4e9a4352027d08709cab73a0d
Samantha-Grace <sgrace732@gmail.com>
2021-04-01 08:17:46 -0600

    type conversion

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	//cards := newDeck()
+
+	//hand, remainingCards := deal(cards, 5)
+
+	//hand.print()
+	//remainingCards.print()
+	greeting := "Hi there!"
+	fmt.Println([]byte(greeting))
+
+}
